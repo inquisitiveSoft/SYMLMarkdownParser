248eea462e4d946f7d23bab482c8753053bbeb83
Harry Jordan <harry@inquisitivesoftware.com>
2015-03-10 17:54:26 +0000

    Tinkered with tests

--- Tests/Markdown Parser Tests.swift
+++ Tests/Markdown Parser Tests.swift
@@ -12,16 +12,6 @@ import SwiftyJSON
 
 
 class Markdown_Parser_Tests: XCTestCase {
-
-//    override func setUp() {
-//        super.setUp()
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//    
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
 	
 	func exampleText() -> String {
 		let exampleDocumentURL = NSBundle.mainBundle().URLForResource("README", withExtension: "md")!
@@ -34,8 +24,8 @@ class Markdown_Parser_Tests: XCTestCase {
         // This is an example of a functional test case.
 		// Parsing without any input
 		let initialParseState = SYMLDefaultMarkdownParserState()
-		SYMLParseMarkdown(nil, nil, initialParseState, nil);
-
+		let resultingState = SYMLParseMarkdown(nil, nil, initialParseState, nil);
+		
 		// Test without an attributed collection to parse into
 		SYMLParseMarkdown(exampleText(), nil, initialParseState, nil);
     }
